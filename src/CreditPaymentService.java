902a6afa8ca52fe091098536f74386d96a653bd6
mayer72 <mayer7272@mail.ru>
2023-05-18 07:51:56 +0500

    auto-formatting

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,9 +2,9 @@ public class CreditPaymentService {
     public int calculate(int term, int credit) {
         double sum;
         double percent = 9.99;
-        double x = Math.pow ((1 + (percent/12/100)), term * 12);
-        sum = credit * (percent/12/100) * x / (x - 1);
-         return (int) sum;
+        double x = Math.pow((1 + (percent / 12 / 100)), term * 12);
+        sum = credit * (percent / 12 / 100) * x / (x - 1);
+        return (int) sum;
     }
 
 }
